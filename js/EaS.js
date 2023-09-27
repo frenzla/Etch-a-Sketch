f13adfefb9244d8222b949a7597fd046d685b0f3
Frenzla <86185774+frenzla@users.noreply.github.com>
2023-09-27 19:15:04 +0200

    Grid created

--- js/EaS.js
+++ js/EaS.js
@@ -1,7 +1,28 @@
 const nbSquares = 16;
 
-const blockContainer = document.querySelector('div.container');
+function createGrid() {
+    //Select container
+    const container = document.querySelector('div.container');
 
-const block = document.createElement('div');
-block.classList.add('block');
-blockContainer.appendChild(block);
+    //Loop for vertical divs
+    for (let k=0; k<nbSquares; k++) {
+        const blockV = document.createElement('div');
+        blockV.classList.add('blockV');
+        container.appendChild(blockV);
+        
+        //Loop for horizontal divs
+        for (let i=0; i<nbSquares; i++) {
+            const blockO = document.createElement('div');
+            blockO.classList.add('blockO');
+            blockV.appendChild(blockO);
+        }
+
+    }
+}
+
+createGrid();
+
+//get width of container
+const containerWidth = parseInt((window.getComputedStyle(document.querySelector('div.container')).getPropertyValue("width")),10);
+
+
