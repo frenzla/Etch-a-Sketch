946502f6eb222c3439b85321aa6ee08a394d3b02
Frenzla <86185774+frenzla@users.noreply.github.com>
2023-09-27 15:43:19 +0200

    setup files and first elements: container & first div block

---
+++ js/EaS.js
@@ -0,0 +1,7 @@
+const nbSquares = 16;
+
+const blockContainer = document.querySelector('div.container');
+
+const block = document.createElement('div');
+block.classList.add('block');
+blockContainer.appendChild(block);
